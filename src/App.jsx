2fa2da091422254c67f0399010d751c0e1a9f2bd
Diego Galdino <68260569+diegogaldino-dev@users.noreply.github.com>
2022-09-23 09:31:28 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {Outlet } from 'react-router-dom';
+import NavBar from './components/NavBar';
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">      
+        <NavBar />
+        <Outlet />
+    </div>
+  );
+}
+
+export default App
